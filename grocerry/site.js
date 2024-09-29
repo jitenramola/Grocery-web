443dba3a24fcda4f9b802324f21d4e0d0426ee1b
jitenramola <146380673+jitenramola@users.noreply.github.com>
2024-09-29 13:00:16 +0530

    Add files via upload

---
+++ grocerry/site.js
@@ -0,0 +1,31 @@
+function fruit()
+{
+        <div class="products-container-main">
+                    <div class="cat-header"></div>
+                        <div class="products-container-main-cards">
+                            <div class="product-card"><span
+                                    style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;">
+                                    <span
+                                        style="box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;">
+                                        <img
+                                            alt="" aria-hidden="true"
+                                            src="/images/apple.jfif"
+                                            style="display: block; max-width: 200px; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;">
+                                        </span>
+                                        <img
+                                        alt="Apple"
+                                        src="/images/apple.jfif"
+                                        decoding="async" data-nimg="intrinsic" class="product-image"
+                                        style="position: absolute; inset: 0px; box-sizing: border-box; padding: 0px; border: none; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
+                                       ></span>
+                                <div class="card-price-footer">
+                                    <p class="product-name">Apple</p>
+                                    <div class="sale-price-container">
+                                        <p class="product-price">Rs.40</p>
+                                    </div>
+                                </div>
+                                <div class="card-btn-footer"><button class="card-btn-atc">Add to Cart</button></div>
+                            </div>
+                    </div>
+            </div> 
+}
